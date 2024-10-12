fe3520f60c6bfb0ebc8ef143b12cac780f880bbb
liticiakouam <anzeliticia@gmail.com>
2024-10-12 21:00:46 +0100

    Confiuration de la base de donnee

---
+++ src/main/java/com/team/mekit/MekitApplication.java
@@ -0,0 +1,13 @@
+package com.team.mekit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MekitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MekitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team/mekit/MekitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.mekit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MekitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
