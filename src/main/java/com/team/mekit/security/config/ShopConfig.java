ae9b31fbc2540bb968b53f28eb92e42967da5d51
liticiakouam <anzeliticia@gmail.com>
2024-10-17 04:13:38 +0100

    Cors permision

--- src/main/java/com/team/mekit/security/config/ShopConfig.java
+++ src/main/java/com/team/mekit/security/config/ShopConfig.java
@@ -33,9 +33,15 @@ public class ShopConfig {
     private final ShopUserDetailsService userDetailsService;
     private final JwtAuthEntryPoint authEntryPoint;
 
-    private static final List<String> NON_SECURED_URLS =
-            List.of("/api/products/all", "/api/products/{productId}/product", "/api/users/recommander/add", "/api/users/seller/add", "/swagger-ui/index.html");
-
+    private static final List<String> NON_SECURED_URLS = List.of(
+            "/api/products/all",
+            "/api/products/{productId}/product",
+            "/api/users/recommander/add",
+            "/api/users/seller/add",
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-ui/index.html"
+    );
 
     @Bean
     public ModelMapper modelMapper() {
